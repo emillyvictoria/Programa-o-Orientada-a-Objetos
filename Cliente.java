9d9a5fd08d2034ff008c7ef1d3b1d9d33000d5e8
Emilly Victoria <82069194+emillyvictoria@users.noreply.github.com>
2023-05-01 14:56:12 -0300

    up

---
+++ Cliente.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author Emilly
+ */
+public class Cliente extends Pessoa{
+    private double valorDivida;
+    private int anoNascim;
+
+    public Cliente(String nome, int anoNascim, String sexo, double valorDivida, int idade) {
+        super(nome, idade, sexo);
+        this.valorDivida = valorDivida;
+        this.anoNascim = anoNascim;
+    }
+
+    public double getValorDivida() {
+        return valorDivida;
+    }
+
+    public int getAnoNascim() {
+        return anoNascim;
+    }
+
+    public void setValorDivida(double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
+
+    public void setAnoNascim(int anoNascim) {
+        this.anoNascim = anoNascim;
+    }
+    
+    @Override
+    public String toString(){
+        return """
+               \nCliente
+               Nome: """ + getNome()
+                +"\nIdade: " + getIdade ()
+                +"\nSexo: " + getSexo ()
+                +"\nAno de Nascimento: " + anoNascim
+                +"\nValor da Divida: " + valorDivida;
+    }
+    
+    
+}
